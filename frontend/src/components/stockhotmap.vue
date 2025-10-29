b6b33c3f8985e7cf61622e8d95eea2e48620961a
ArvinLovegood <arvinlovegood@163.com>
2025-10-29 16:18:29 +0800

    feat(stockhotmap):启用百度股市通和摸鱼网页标签页
    - 取消注释百度股市通标签页，恢复其功能
    - 取消注释摸鱼标签页，恢复其功能
    -保持其他标签页配置不变

--- frontend/src/components/stockhotmap.vue
+++ frontend/src/components/stockhotmap.vue
@@ -9,18 +9,18 @@ import EmbeddedUrl from "./EmbeddedUrl.vue";
     <n-tab-pane name="选股通" tab="选股通">
       <embedded-url url="https://xuangutong.com.cn"  :height="'calc(100vh - 252px)'"/>
     </n-tab-pane>
-<!--    <n-tab-pane name="百度股市通" tab="百度股市通">-->
-<!--      <embedded-url url="https://gushitong.baidu.com"  :height="'calc(100vh - 252px)'"/>-->
-<!--    </n-tab-pane>-->
+    <n-tab-pane name="百度股市通" tab="百度股市通">
+      <embedded-url url="https://gushitong.baidu.com"  :height="'calc(100vh - 252px)'"/>
+    </n-tab-pane>
     <n-tab-pane name="东财大盘星图" tab="东财大盘星图">
       <embedded-url url="https://quote.eastmoney.com/stockhotmap/"  :height="'calc(100vh - 252px)'"/>
     </n-tab-pane>
     <n-tab-pane name="TopHub" tab="TopHub(今日热榜)">
       <embedded-url url="https://tophub.today/c/finance"  :height="'calc(100vh - 252px)'"/>
     </n-tab-pane>
-<!--    <n-tab-pane name="摸鱼" tab="摸鱼">-->
-<!--      <embedded-url url="https://996.ninja/"  :height="'calc(100vh - 252px)'"/>-->
-<!--    </n-tab-pane>-->
+    <n-tab-pane name="摸鱼" tab="摸鱼">
+      <embedded-url url="https://996.ninja/"  :height="'calc(100vh - 252px)'"/>
+    </n-tab-pane>
 
 
     <n-tab-pane name="欢迎推荐更多有趣的财经网页" tab="欢迎推荐更多有趣的财经网页">
